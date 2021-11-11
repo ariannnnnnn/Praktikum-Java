6afa9eeb04cd49018d889899c28a3d8dfe03c80e
Arian Febrian <88012298+r3nyah@users.noreply.github.com>
2021-11-11 08:20:36 +0700

    Add files via upload

--- UKL-PBO/src/Lelang/Main.java
+++ UKL-PBO/src/Lelang/Main.java
@@ -9,8 +9,11 @@ package Lelang;
  *
  * @author Arian
  */
+//import java.io.PrintStream;
 import java.util.Scanner;
-public class Main {
+
+//import javax.sound.sampled.SourceDataLine;
+public class Main{
     /*VAR*/
     static Masyarakat masyarakat = new Masyarakat();
     static Barang barangLelang = new Barang();
@@ -24,6 +27,8 @@ public class Main {
     static int Pilihan;
 
     static String loginName = "";
+    static String loginAddress = "";
+    static String loginNumber = "";
     static bot opponent = new bot();
     static int currentHarga;
 
@@ -32,98 +37,108 @@ public class Main {
     /*VAR*/
 
     public static void main(String[] args) {
+        
         login();
-        askGeneratebarang();
+      //askGeneratebarang();
         askAjukanBarang();
-        System.out.println("Daftar barang");
-        System.out.println();//jarak
+        System.out.println("Item List :");
+        System.out.println();
         barangLelang.tampilBarang();
 
-        System.out.print("barang mana yang ingin anda tawar ? : ");
+        System.out.print("which item would you like to bargain? : ");
         Pilihan = userInput.nextInt();
         if (Pilihan > barangLelang.idBarang.size() || !barangLelang.getStatus(Pilihan)){
-            System.out.println("nomor barang tidak valid");
+                  System.out.println("ERROR occured :::: java programm has stopped!!!");
+                  System.out.println("ERROR occured :::: java programm has stopped!!!");
+                  System.out.println("ERROR occured :::: java programm has stopped!!!");
+                  System.exit(1);
+           
+                 
         }else{
-            System.out.println("Barang : " + barangLelang.getNamaBarang(Pilihan));
-            System.out.println("harga awal : " + barangLelang.getHargaAwal(Pilihan));
+            System.out.println("Name : " + barangLelang.getNamaBarang(Pilihan));
+            System.out.println("Initial price : " + barangLelang.getHargaAwal(Pilihan));
             System.out.println("Status : " + barangLelang.getStatus(Pilihan));
             barangLelang.addPenawar(masyarakat.getIdMasyarakat(masyarakat.getIndexFromName(loginName)));
             barangLelang.overrideHargaTawar();
             barangLelang.addHargaTawar(Integer.parseInt(barangLelang.getHargaAwal(Pilihan)));
 
             lelangStart();
+            pemenang();
         }
-        pemenang();
     }
 
     static boolean UserAnswerIsYes(String input){
-        return input.equalsIgnoreCase("y");
+        return input.equalsIgnoreCase("yes");
     }
 
     static void login(){
-        System.out.println("++ LOGIN ++");
-        System.out.print("Nama : ");
+        System.out.println("++ LOGIN --");
+        System.out.print("Name\t\t: ");
         String nama = userInput.nextLine();
         loginName = nama;
-        System.out.print("Alamat : ");
+        System.out.print("Address\t\t: ");
         String alamat = userInput.nextLine();
-        System.out.print("telp : ");
+        loginAddress = alamat;
+        System.out.print("Phone number\t: ");
         String telp = userInput.nextLine();
+        loginNumber = telp;
+        System.out.println();
         masyarakat.Masyarakat(nama, alamat, telp);
-    }
-
-    static void askGeneratebarang(){
-        //ask for generate
-        System.out.print("Generate Barang ? (y/n) : ");
-        user_input = userInput.nextLine();
-
-        if (UserAnswerIsYes(user_input)){
-            generatebarang();
-            System.out.println("Barang Lelang generated");
-        }else{
-            System.out.println("oke");
         }
-    }
+
+  //  static void askGeneratebarang(){
+        //ask for generate
+      //  System.out.print("Generate Barang ? (Yes / No) : ");
+      //  user_input = userInput.nextLine();
+
+       // if (UserAnswerIsYes(user_input)){
+          //  generatebarang();
+         //   System.out.println("Auction Items generated");
+       // }else{
+          //  System.out.println("kk");
+       // }
+    //}
 
     static void askAjukanBarang(){
         //ask if you want to create barang yourself
-        System.out.print("Ajukan barang untuk lelang? (y/n) : ");
+        System.out.print("Submit an item for auction? (Yes /  No) : ");
         user_input = userInput.nextLine();
         if (UserAnswerIsYes(user_input)){
+            generatebarang();
             petugas.doYourJob(barangLelang);
-            System.out.println("barang ditambahkan");
+            System.out.println("Item Added");
+           System.out.println("Auction Items generated");
         }else{
-            System.out.println("oke");
+           generatebarang();
+           //System.out.println("kk");
         }
     }
 
     static void lelangStart() {
         do {
 
-            /*logic here*/
-            //our turn
+            //human
             currentHarga = barangLelang.getHargaTertinggi();
-            System.out.println("harga Saat Ini : " + currentHarga);
+            System.out.println("Current price : " + currentHarga);
             System.out.println();//jarak
-            System.out.print("Masukkan Harga : ");
+            System.out.print("Input price : ");
             int harga = userInput.nextInt();
-
-            System.out.println();//jarak
-
+            //System.out.println();
             if (harga <= barangLelang.getHargaTertinggi()){
-                System.out.println("harga anda terlalu rendah");
+                System.out.println("Input bigger number");
             }else {
                 barangLelang.addHargaTawar(harga);
                 user_harga = harga;
             }
-            //System.out.println();//jarak
+            System.out.println();
 
             currentHarga = barangLelang.getHargaTertinggi();
 
-            //bot turn
+            //bot
             bot_harga = opponent.generateHarga(currentHarga);
             if (bot_harga <= barangLelang.getHargaTertinggi()){
-                System.out.println("Bot :  skip dlu");
+                System.out.println("Bot :  Skip!!");
+                
             }else {
                 barangLelang.addHargaTawar(bot_harga);
                 System.out.println("Bot : " + bot_harga);
@@ -132,39 +147,40 @@ public class Main {
             System.out.println();//jarak
             /*logic here*/
 
-            System.out.print("apakah anda ingin menawar lagi ? (Y/T) : ");
+            System.out.print("Do you want to bid again? (Yes / No) : ");
             jawab = ulangi.nextLine();
 
-        } while (UserAnswerIsYes(jawab));
+        }while (UserAnswerIsYes(jawab));
     }
 
     static void generatebarang() {
-        barangLelang.addNamaBarang("Sabun wangy wangy");
-        barangLelang.addHargaAwal("10000");
-        barangLelang.addHargaTawar(10000);
+        barangLelang.addNamaBarang("Ring");
+        barangLelang.addHargaAwal("100000");
+        barangLelang.addHargaTawar(100000);
         barangLelang.setStatus(true);
 
-        barangLelang.addNamaBarang("Artefak");
-        barangLelang.addHargaAwal("400000");
-        barangLelang.addHargaTawar(400000);
+        barangLelang.addNamaBarang("Flowers");
+        barangLelang.addHargaAwal("20000");
+        barangLelang.addHargaTawar(20000);
         barangLelang.setStatus(true);
 
-        barangLelang.addNamaBarang("keyboard");
-        barangLelang.addHargaAwal("200000");
-        barangLelang.addHargaTawar(200000);
-        barangLelang.setStatus(true);
     }
 
     static void pemenang(){
         barangLelang.setStatus(Pilihan, false);
         if (currentHarga == user_harga){
-            System.out.println("Pemenang " + barangLelang.getNamaBarang(Pilihan));
-            System.out.println("Adalah : " + loginName);
-            System.out.println("Dengan harga : " + currentHarga);
+            System.out.println();
+            System.out.print("Winner " + barangLelang.getNamaBarang(Pilihan));
+            System.out.print(" is " + loginName + " For : " + currentHarga);
+            System.out.println();
+            System.out.println("Name\t\t= "+loginName);
+            System.out.println("Address\t\t= "+loginAddress);
+            System.out.println("Phone Number\t= "+loginNumber);
+            
         }else {
-            System.out.println("Pemenang " + barangLelang.getNamaBarang(Pilihan));
-            System.out.println("Adalah : BOT");
-            System.out.println("Dengan harga : " + currentHarga);
+            System.out.print("Winner " + barangLelang.getNamaBarang(Pilihan));
+            System.out.print(" is BOT");
+            System.out.println();
         }
     }
 }
